7dd4bcd939e8cb0496392c8de40cfa34be4dd502
JadeCouch <JadeCouch@users.noreply.github.com>
2022-01-10 19:56:25 +0200

    Fundamentals 1 Code Challenge 3

--- js-fundamentals-challenges/fundamentals-1.js
+++ js-fundamentals-challenges/fundamentals-1.js
@@ -2,6 +2,7 @@
 
 //coding challenge 1
 
+/*
 let markWeight = 78;
 let markHeight = 1.69;
 let johnWeight = 92;
@@ -15,11 +16,38 @@ console.log(johnBMI);
 
 const markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+*/
 
 //coding challenge 2
 
+/*
 if (markHigherBMI) {
   console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
 } else {
   console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+}
+*/
+
+//coding challenge 3
+
+const averageScoreDolphins = (97 + 112 + 101) / 3;
+const averageScoreKoalas = (109 + 95 + 106) / 3;
+
+console.log(averageScoreDolphins, averageScoreKoalas);
+
+if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
+  console.log("Dolphins are the winners!");
+} else if (
+  averageScoreKoalas > averageScoreDolphins &&
+  averageScoreKoalas >= 100
+) {
+  console.log("Koalas are the winners!");
+} else if (
+  averageScoreDolphins === averageScoreKoalas &&
+  averageScoreDolphins >= 100 &&
+  averageScoreKoalas >= 100
+) {
+  console.log("It's a draw!");
+} else {
+  console.log("No winner!");
 }
